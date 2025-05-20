f0b91f9ae9989ef446509078e0d408c793a9df17
Valentine Ncube <valentinemelly77@gmail.com>
2025-05-21 01:00:29 +1200

    Added getInsuranceInfo function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [image, setImage] = useState(null) // State to hold the selected image
-  const [preview, setPreview] = useState(null) // State to hold the image preview
-  const [prediction, setPrediction] = useState(null) // State to hold the prediction result
+  const [image, setImage] = useState(null) // Holds the selected image
+  const [preview, setPreview] = useState(null) // Holds the image preview
+  const [prediction, setPrediction] = useState(null) // Holds the prediction result
   const [loading, setLoading] = useState(false) // State to manage loading state
-}
 
+  // Function to determine premium information
+  const getInsuranceInfo = (vehicleType) => {
+    const premiumInfo = {
+      Suv: 'Premium starts at $300/month',
+      Truck: 'Premium starts at $400/month',
+      Sedan: 'Premium starts at $200/month',
+    }
+
+    // Normalize the vehicle type to match the keys in premiumInfo, ensures the function is case-insensitive
+    const normalizedType =
+      vehicleType.charAt(0).toUpperCase() + vehicleType.slice(1).toLowerCase()
+    return premiumInfo[normalizedType] || 'No premium information available'
+  }
+}
 export default App
