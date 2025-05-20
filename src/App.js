6b0bf31449c08b9fb84fc17b9b6dfe04cde6cc92
Valentine Ncube <valentinemelly77@gmail.com>
2025-05-21 00:44:18 +1200

    Setup component state and imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [image, setImage] = useState(null) // State to hold the selected image
+  const [preview, setPreview] = useState(null) // State to hold the image preview
+  const [prediction, setPrediction] = useState(null) // State to hold the prediction result
+  const [loading, setLoading] = useState(false) // State to manage loading state
 }
 
-export default App;
+export default App
